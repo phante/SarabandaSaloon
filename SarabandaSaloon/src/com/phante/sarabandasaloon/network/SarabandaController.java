ea7fca9ea03a8f989110a1146f68fc15b0ac816d
elvisdeltedesco <elvisdeltedesco@192.168.0.2>
2015-07-13 21:47:51 +0200

    Modificata l'intestazione della licenza

--- SarabandaSaloon/src/com/phante/sarabandasaloon/network/SarabandaController.java
+++ SarabandaSaloon/src/com/phante/sarabandasaloon/network/SarabandaController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 deltedes.
+ * Copyright 2015 Elvis Del Tedesco
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
